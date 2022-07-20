1e9515ee407b77e9d0638b1af4b33aedba1b7f42
Armaghanhashmi11 <armaghan.hashmi87@gmail.com>
2022-07-20 12:49:09 -0700

    static

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import "../styles/globals.css";
 import Layout from "./Layout/Layout";
 
-MyApp.getInitialProps = async () => {
-  let headers = { Authorization: `Barear ${process.env.token}` };
-  const res = await fetch("http://localhost:1337/api/headers", {
-    headers: headers,
-  });
-  const headerLinks = await res.json();
-  return { headerLinks };
-};
+// MyApp.getInitialProps = async () => {
+//   let headers = { Authorization: `Barear ${process.env.token}` };
+//   const res = await fetch("http://localhost:1337/api/headers", {
+//     headers: headers,
+//   });
+//   const headerLinks = await res.json();
+//   return { headerLinks };
+// };
 function MyApp({ Component, pageProps, headerLinks }) {
   return (
     <Layout

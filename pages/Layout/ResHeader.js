93a73129bac0140efabee14f27bfe8842c9267a7
Armaghanhashmi11 <armaghan.hashmi87@gmail.com>
2022-07-20 12:57:31 -0700

    static

--- pages/Layout/ResHeader.js
+++ pages/Layout/ResHeader.js
@@ -43,7 +43,7 @@ const ResHeader = ({ headerLinks }) => {
           </div>
           <div
             className={
-              
+
               expanded
                 ? `transition-height duration-200 ease-in bg-lightGrey  block w-full text-lg text-center pt-6 mt-[11px]  overflow-y-hidden top-[50px] left-0 md:inline md:float-right h-[100vh] md:text-right  md:h-auto  md:p-0 md:m-0  ${
                     route.asPath == "/" ? " md:bg-lightGrey" : "md:bg-white"
@@ -53,10 +53,9 @@ const ResHeader = ({ headerLinks }) => {
                   }`
             }
           >
-            {headerLinks &&
-              headerLinks.data.map((link, index) => (
+            
                 <Link
-                  href={link.attributes.link}
+                  href='/OurServices'
                   className="w-full text-white py-[13px] px-[10px] mr-[30px] pb-[13px]"
                   key={index}
                 >
@@ -66,10 +65,66 @@ const ResHeader = ({ headerLinks }) => {
                       setExpanded(!expanded);
                     }}
                   >
-                    {link.attributes.name}
+                   Our Services
                   </a>
                 </Link>
-              ))}
+                <Link
+                  href='About'
+                  className="w-full text-white py-[13px] px-[10px] mr-[30px] pb-[13px]"
+                  key={index}
+                >
+                  <a
+                    className="block my-4 md:my-0   md:inline-block text-sm md:ml-6"
+                    onClick={() => {
+                      setExpanded(!expanded);
+                    }}
+                  >
+                   About Us
+                  </a>
+                </Link>
+                <Link
+                  href='OurClient'
+                  className="w-full text-white py-[13px] px-[10px] mr-[30px] pb-[13px]"
+                  key={index}
+                >
+                  <a
+                    className="block my-4 md:my-0   md:inline-block text-sm md:ml-6"
+                    onClick={() => {
+                      setExpanded(!expanded);
+                    }}
+                  >
+                   Our Clients
+                  </a>
+                </Link>
+                <Link
+                  href='Contact'
+                  className="w-full text-white py-[13px] px-[10px] mr-[30px] pb-[13px]"
+                  key={index}
+                >
+                  <a
+                    className="block my-4 md:my-0   md:inline-block text-sm md:ml-6"
+                    onClick={() => {
+                      setExpanded(!expanded);
+                    }}
+                  >
+                   Contact Us
+                  </a>
+                </Link>
+                <Link
+                  href=''
+                  className="w-full text-white py-[13px] px-[10px] mr-[30px] pb-[13px]"
+                  key={index}
+                >
+                  <a
+                    className="block my-4 md:my-0   md:inline-block text-sm md:ml-6"
+                    onClick={() => {
+                      setExpanded(!expanded);
+                    }}
+                  >
+                   
+                  </a>
+                </Link>
+              
 
             <button className=" md:bg-blue-500 md:text-white text-sm md:w-32 md:h-10 md:text-base md:py-2 md:px-4 md:rounded-[30px] md:ml-8 lg:ml-14 md:mr-4">
               Get Started
